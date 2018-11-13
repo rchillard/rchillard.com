29bd73d7a14c321f9bc49b1ddd5ddcf1928a86ea
RCH <rchillard@gmail.com>
2018-11-12 22:42:58 -0600

    Test out initial pages and Gatsby setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{ color: `purple` }}>
+        <Link to="/contact/">Contact</Link>
+        <Header headerText="About Gatsby" />
+        <Header headerText="WTF Props!!" />
+        Hello Gatsby!
+    </div>
+)
