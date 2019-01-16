6db10224e084dfd933a95e575ed551ba24dbff25
RCH <rchillard@gmail.com>
2019-01-16 02:37:26 -0600

    Update layout component for initial background setup

--- src/components/background.js
+++ src/components/background.js
@@ -1,25 +1,25 @@
-// import React from "react"
-// import styles from "./background.module.css"
+import React from "react"
+import styles from "./background.module.css"
 
-// const BackgroundColor = props => (
-//     // var hour = new Date().getHours()
+const BackgroundColor = props => (
+    var hour = new Date().getHours()
 
-//     <div className={styles.dawn}>
-//         <p>So what?</p>
-//     </div>
-//     // if (hour >= 5 && hour <= 7) {
-//     //     BackgroundColor.classList.add('dawn')
-//     // } else if (hour > 7 && hour < 18) {
-//     //     bg.classList.add('day')
-//     // } else if (hour >= 18 && hour <= 20) {
-//     //     bg.classList.add('dusk')
-//     // } else {
-//     //     bg.classList.add('night')
-//     // }
-//   )
+    <div className={styles.dawn}>
+        <p>So what?</p>
+    </div>
+    if (hour >= 5 && hour <= 7) {
+        BackgroundColor.classList.add('dawn')
+    } else if (hour > 7 && hour < 18) {
+        bg.classList.add('day')
+    } else if (hour >= 18 && hour <= 20) {
+        bg.classList.add('dusk')
+    } else {
+        bg.classList.add('night')
+    }
+  )
 
-// export default ({ children }) => (
-//     <BackgroundColor>
+export default ({ children }) => (
+    <BackgroundColor>
 
-//     </BackgroundColor>
-// )
+    </BackgroundColor>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
-import React from "react"
-import { css } from "react-emotion"
-import { StaticQuery, Link, graphql } from "gatsby"
+import React from "react";
+import { css } from "react-emotion";
+import { StaticQuery, Link, graphql } from "gatsby";
+import layoutStyles from "./layout.module.css";
 
-import { rhythm } from "../utils/typography"
+import { rhythm } from "../utils/typography";
 
 export default ({ children }) => (
-    <StaticQuery
+  <StaticQuery
     query={graphql`
       query {
         site {
@@ -14,38 +15,39 @@ export default ({ children }) => (
           }
         }
       }
-    `
-}
-
-render={data => (
-  <div
-    className={css`
-      margin: 0 auto;
-      max-width: 900px;
-      padding: ${rhythm(2)};
-      padding-top: ${rhythm(1.5)};
     `}
-  >
-    <Link to={`/`}>
-      <h3
-        className={css`
-          margin-bottom: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-      >
-        {data.site.siteMetadata.title}
-      </h3>
-    </Link>
-    <div className={css`float: right;`}>
-      {/* <Link to={`/lessons/`} className={css`margin-right: 1em`}>Lessons</Link>
-      <Link to={`/projects/`} className={css`margin-right: 1em`}>Projects</Link>
-      <Link to={`/resources/`} className={css`margin-right: 1em`}>Resources</Link> */}
-      <Link to={`/about/`}>About</Link>
-    </div>
-    {children}
-  </div>
-  
-  )}
-/>
-)
+    render={data => (
+      <div className={layoutStyles.day}>
+        <div
+          className={css`
+            background: white;
+            margin: 0 auto;
+            max-width: 900px;
+            padding: ${rhythm(2)};
+            padding-top: ${rhythm(1.5)};
+          `}
+        >
+          <Link to={`/`}>
+            <h3
+              className={css`
+                margin-bottom: ${rhythm(2)};
+                display: inline-block;
+                font-style: normal;
+              `}
+            >
+              {data.site.siteMetadata.title}
+            </h3>
+          </Link>
+          <div
+            className={css`
+              float: right;
+            `}
+          >
+            <Link to={`/about/`}>About</Link>
+          </div>
+          {children}
+        </div>
+      </div>
+    )}
+  />
+);
