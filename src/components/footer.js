d993faed6600c6a35c448ca512ba0cfe3229d9fb
Ryan <rchillard@gmail.com>
2019-03-11 21:53:29 -0500

    Fix issue with footer+background padding in CSS

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
 import style from "./footer.module.css";
-// import github from "../images/github.svg";
 import launchpad from "../images/rch-launchpad.svg";
 import github from "../images/rch-github.svg";
 import npm from "../images/rch-npm.svg";
 import docker from "../images/rch-docker.svg";
-// import npm from "../images/npm.svg";
-// import docker from "../images/docker.svg";
 
 class Footer extends Component {
   render() {
     return (
-      <div className={style.footer}>
-        <img src={launchpad} alt="RCH Launchpad Icon" className={style.pad} />
-        <a href="https://github.com/rchillard">
-          <img src={github} alt="RCH GitHub Icon" className={style.special} />
-        </a>
-        <a href="https://www.npmjs.com/~rchillard">
-          <img src={npm} alt="RCH NPM Icon" className={style.icon} />
-        </a>
-        <a href="https://hub.docker.com/u/rchillard/">
-          <img src={docker} alt="RCH Docker Icon" className={style.icon} />
-        </a>
+      <div className={style.ground}>
+        <div className={style.footer}>
+          <img src={launchpad} alt="RCH Launchpad Icon" className={style.pad} />
+          <a href="https://github.com/rchillard">
+            <img src={github} alt="RCH GitHub Icon" className={style.special} />
+          </a>
+          <a href="https://www.npmjs.com/~rchillard">
+            <img src={npm} alt="RCH NPM Icon" className={style.icon} />
+          </a>
+          <a href="https://hub.docker.com/u/rchillard/">
+            <img src={docker} alt="RCH Docker Icon" className={style.icon} />
+          </a>
+        </div>
       </div>
     );
   }
