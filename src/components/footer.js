a5a0c24770cf21ff1570056af95654ceff04f72d
RCH <rchillard@gmail.com>
2019-03-12 23:16:18 -0500

    Restructure CSS in footer for absolute position and add media query

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import style from "./footer.module.css";
-// import launchpad from "../images/rch-launchpad.svg";
+import launchpad from "../images/rch-launchpad.svg";
 import github from "../images/rch-github.svg";
 import npm from "../images/rch-npm.svg";
 import docker from "../images/rch-docker.svg";
@@ -8,19 +8,17 @@ import docker from "../images/rch-docker.svg";
 class Footer extends Component {
   render() {
     return (
-      <div className={style.ground}>
-        <div className={style.footer}>
-        {/* <img src={launchpad} alt="RCH Launchpad Icon" className={style.pad} /> */}
-          <a href="https://github.com/rchillard">
-            <img src={github} alt="RCH GitHub Icon" className={style.special} />
-          </a>
-          <a href="https://www.npmjs.com/~rchillard">
-            <img src={npm} alt="RCH NPM Icon" className={style.icon} />
-          </a>
-          <a href="https://hub.docker.com/u/rchillard/">
-            <img src={docker} alt="RCH Docker Icon" className={style.icon} />
-          </a>
-        </div>
+      <div className={style.footer}>
+        <img src={launchpad} alt="RCH Launchpad Icon" className={style.pad} />
+        <a href="https://github.com/rchillard">
+          <img src={github} alt="RCH GitHub Icon" className={style.github} />
+        </a>
+        <a href="https://www.npmjs.com/~rchillard">
+          <img src={npm} alt="RCH NPM Icon" className={style.npm} />
+        </a>
+        <a href="https://hub.docker.com/u/rchillard/">
+          <img src={docker} alt="RCH Docker Icon" className={style.docker} />
+        </a>
       </div>
     );
   }
