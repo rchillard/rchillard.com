ad942a353b9dac4a064cda531141eb09c2dbfb52
RCH <20631917+rchillard@users.noreply.github.com>
2024-12-31 07:32:49 -0600

    Create skeleton of new site using sane.html, delete old Gatsby site

--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography"
-import noriegaTheme from "typography-theme-noriega"
-
-const typography = new Typography(noriegaTheme)
-
-export default typography
-export const rhythm = typography.rhythm
