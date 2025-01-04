58211abeca2de5a76e447974f8b10782e174c6ad
RCH <20631917+rchillard@users.noreply.github.com>
2025-01-04 12:14:05 -0600

    Complete basic bash script for building with CSS cache busting

--- build.sh
+++ build.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
+# build.sh
 
-# File to search and update
-FILE="test-index.html"
+SRC_DIR="src/."
+DEST_DIR="build/"
 
-# https://blog.codinghorror.com/parsing-html-the-cthulhu-way/
-# Search pattern
-PATTERN='link rel="stylesheet" href="css/styles.css"'
+echo "Building site..."
+
+# Delete the old directory and create a new one
+rm --recursive --force "$DEST_DIR"
+mkdir --parents "$DEST_DIR"
+
+# Copy all files from source to destination directory
+cp --recursive --verbose "$SRC_DIR/" "$DEST_DIR/"
+echo "Files copied to $DEST_DIR."
 
 # Get the current Unix timestamp
 TIMESTAMP=$(date +%s)
 
+# Search pattern, relevant: https://blog.codinghorror.com/parsing-html-the-cthulhu-way/
+PATTERN='link rel="stylesheet" href="css/styles.css"'
+
 # Calculate a new cache-busted value for the stylesheet link
 NEW_HREF="css/styles.css?v=$TIMESTAMP"
+echo "CSS cache will be busted by adding this query parameter: $NEW_HREF"
 
-echo $NEW_HREF
+FILE_COUNT_CSS_UPDATED=0
 
-# Check if the file exists
-if [[ -f "$FILE" ]]; then
-  # Search for the pattern and update it
-  if grep -e "$PATTERN" "$FILE"; then
-    sed -i "s|<link rel=\"stylesheet\" href=\"css/styles.css\">|<link rel=\"stylesheet\" href=\"$NEW_HREF\">|" "$FILE"
-    echo "Updated line with timestamp $TIMESTAMP."
+# Create an array containing all .html files
+mapfile -t html_files_array < <(find ./build -maxdepth 1 -type f -name "*.html")
+
+FILE_COUNT_CSS_POSSIBLE=${#html_files_array[@]}
+
+# Iterate over the array, checking each file for the pattern and updating if present
+for html_file in "${html_files_array[@]}"; do
+  echo "Found file: $html_file and will attempt to find line..."
+  if grep --regexp="$PATTERN" "$html_file"; then
+    echo "Found line: $PATTERN and will attempt to update..."
+    sed --in-place "s|<link rel=\"stylesheet\" href=\"css/styles.css\">|<link rel=\"stylesheet\" href=\"$NEW_HREF\">|" "$html_file"
+    echo "Updated $html_file with $PATTERN to new reference with timestamp $NEW_HREF."
+    ((FILE_COUNT_CSS_UPDATED+=1))
   else
-    echo "No matching line found."
+    echo "No matching line found in $html_file."
   fi
-else
-  echo "File $FILE does not exist."
-fi
+done
+
+echo "Build complete..."
+echo "HTML files updated: $FILE_COUNT_CSS_UPDATED / $FILE_COUNT_CSS_POSSIBLE"
